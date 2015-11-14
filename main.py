fa630d468ba128f9916ea5da71d87a18891d5da5
dana <danasemaniv@gmail.com>
2015-11-14 14:11:27 +0200

    Added function for show/hide password button

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ class Student_health_records_app(QtWidgets.QMainWindow, authorization_ui.Ui_Stud
         self.setupUi(self)
         self.setFixedSize(self.width(), self.height())
 
-
         self.login_error = QtWidgets.QLabel(self)
         self.login_error.move(135, 47)
 
@@ -26,7 +25,7 @@ class Student_health_records_app(QtWidgets.QMainWindow, authorization_ui.Ui_Stud
 
         self.sign_up_button.clicked.connect(self.sign_up)
         self.log_in_button.clicked.connect(self.log_in)
-
+        self.show_pass.clicked.connect(self.show_hide_password)
 
     def sign_up(self):
         self.u = registration_ui.Ui_Students_health_records_registration_ui()
@@ -35,13 +34,14 @@ class Student_health_records_app(QtWidgets.QMainWindow, authorization_ui.Ui_Stud
         dialog.show()
         dialog.exec()
 
-
     def log_in(self):
         login = self.lineEdit.text()
         password = self.lineEdit_2.text()
 
         if self.verification_sign_in(login, password):
+            self.close()
             self.user()
+            self.show()
 
     def user(self):
         self.u = user_ui.Ui_Student_health_records()
@@ -65,22 +65,27 @@ class Student_health_records_app(QtWidgets.QMainWindow, authorization_ui.Ui_Stud
         dialog.exec()
 
     def verification_sign_in(self, login, password):
+        success = True
 
         self.login_error.setText(" ")
         self.password_error.setText(" ")
 
         if self.is_set_label(login):
             self.login_error.setText("This label cannot be empty!")
+            success = False
 
         if self.check_length(password):
             self.password_error.setText("Length must be between 8 and 20 symbols")
+            success = False
 
         if self.is_set_label(password):
             self.password_error.setText("This label cannot be empty!")
+            success = False
 
         self.login_error.adjustSize()
         self.password_error.adjustSize()
 
+        return success
 
     def is_set_label(self, label):
         if len(label) == 0:
@@ -91,3 +96,9 @@ class Student_health_records_app(QtWidgets.QMainWindow, authorization_ui.Ui_Stud
         if len(label) < 8 or len(label) > 20:
             return True
         return False
+
+    def show_hide_password(self):
+        if self.lineEdit_2.echoMode() == 0:
+            self.lineEdit_2.setEchoMode(2)
+        else:
+            self.lineEdit_2.setEchoMode(0)
