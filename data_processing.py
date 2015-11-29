6338ae66a992cb97a1984da83854f7d828a1a394
TarasRudnyk <TarasRudnyk@gmail.com>
2015-11-29 14:02:44 +0200

    Processing user data.

--- data_processing.py
+++ data_processing.py
@@ -1,27 +1,55 @@
 import cx_Oracle
 
+
 def authorize_user(login, password):
-    authorize_result = {"success": False}
-    authorize_result["role"] = 'user'
+    authorize_result = {"success": False,
+                        "role" : 'user'}
 
     con = cx_Oracle.connect('taras/orcl@localhost/orcl')
     cur = con.cursor()
 
-    cur.execute('select user_login, user_password, user_role from clients')
+    cur.execute('SELECT user_login, user_password, user_role FROM clients')
     for result in cur:
         if (login == result[0] and password == result[1]):
             user_role = result[2]
             authorize_result["success"] = True
             authorize_result["role"] = user_role
 
-
     return authorize_result
 
 
-def get_user_diagnoses(username):
-    database_reply = {"login": username}
-    get_user_diagnoses_result = {"success": True}
-    get_user_diagnoses_result.update(database_reply)
-    return get_user_diagnoses_result
+def get_user_diagnoses(login):
+    diagnoses_result = {"diagnose_name": '',
+                        "diagnose_date": '',
+                        "diagnose_doctor": ''}
+
+    diagnose_name = []
+    diagnose_date = []
+    diagnose_doctor = []
+
+
+    con = cx_Oracle.connect('taras/orcl@localhost/orcl')
+    cur = con.cursor()
+
+    cur.execute('SELECT user_card_number FROM clients WHERE user_login = \'{0}\''.format(login))
+    for result_card_number in cur:
+        user_card_number = result_card_number[0]
+
+    cur.execute('SELECT diagnose_number FROM MEDICALCARD WHERE user_card_number = \'{0}\''.format(user_card_number))
+    for result_diagnose_number in cur:
+        diagnose_number = result_diagnose_number[0]
+
+    cur.execute('SELECT diagnose_name, diagnose_date, diagnose_doctor FROM DIAGNOSES WHERE diagnose_number= \'{0}\''.format(diagnose_number))
+    for result_diagnose in cur:
+        diagnose_name.append(result_diagnose[0])
+        diagnose_date.append(result_diagnose[1])
+        diagnose_doctor.append(result_diagnose[2])
+
+        diagnoses_result["diagnose_name"] = diagnose_name
+        diagnoses_result["diagnose_date"] = diagnose_date
+        diagnoses_result["diagnose_doctor"] = diagnose_doctor
+
+    return diagnoses_result
+
 
 
